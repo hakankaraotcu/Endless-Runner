d5c3fb67cc1c3d106fd84648c81726728f44c74b
mbattaloglu <mmbattaloglu@gmail.com>
2022-12-01 01:27:19 +0300

    Touch Controls and Camera Adjustments

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    /*
     public Transform target;
     public Vector3 offset;
     // Start is called before the first frame update
@@ -17,5 +18,12 @@ public class CameraController : MonoBehaviour
     {
         Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, offset.z + target.position.z);
         transform.position = Vector3.Lerp(transform.position, newPosition, 10 * Time.deltaTime);
+    }*/
+
+    public Transform player;
+    public Vector3 offset2;
+    void LateUpdate()
+    {
+        transform.position = new Vector3(Mathf.Lerp(transform.position.x, player.position.x, 10 * Time.deltaTime), transform.position.y, offset2.z + player.position.z);
     }
 }
---
+++ Assets/Scripts/PlayerTouchController.cs
@@ -0,0 +1,158 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class PlayerTouchController : MonoBehaviour
+{
+    public bool jump = false;
+    public bool slide = false;
+
+    public Side side = Side.Mid;
+    
+    private float newXPos = 0f;
+
+    [HideInInspector]
+    public bool swipeLeft, swipeRight, swipeUp, swipeDown;
+    [HideInInspector]
+    public bool continueToMove;
+
+    public float xValue;
+    public float speedDodge;
+    public float jumpPower = 7f;
+    private float x;
+    private float y;
+    public float forwardSpeed = 7f;
+    public float maxSpeed = 15f;
+    private float colHeight;
+    private float colCenterY;
+
+    private float x1, x2, y1, y2; 
+
+    public Animator anim;
+    private CharacterController controller;
+
+    
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+        colHeight = controller.height;
+        colCenterY = controller.center.y;
+        transform.position = Vector3.zero;
+    }
+    
+    void Update()
+    {   
+        if (Input.GetMouseButtonDown(0))
+        {
+            x1 = Input.mousePosition.x;
+            y1 = Input.mousePosition.y;
+            continueToMove = true;
+        }
+
+        if (Input.GetMouseButton(0) && continueToMove)
+        {
+            x2 = Input.mousePosition.x;
+            y2 = Input.mousePosition.y;
+        
+            swipeRight = x2 - x1 > Screen.width / 5;
+            swipeLeft = x2 - x1 < 0 && Mathf.Abs(x2 - x1) > Screen.width / 5;
+            swipeUp = y2 - y1 > Screen.height / 5;
+            swipeDown = y2 - y1 < 0 && Mathf.Abs(y2 - y1) > Screen.height / 5;
+
+            if (swipeLeft)
+            {
+                if (side == Side.Mid)
+                {
+                    newXPos = -xValue;
+                    side = Side.Left;
+                }
+                else if (side == Side.Right)
+                {
+                    newXPos = 0;
+                    side = Side.Mid;
+                }
+                continueToMove = false;
+            }
+
+            else if (swipeRight)
+            {
+                if (side == Side.Mid)
+                {
+                    newXPos = xValue;
+                    side = Side.Right;
+                }
+                else if (side == Side.Left)
+                {
+                    newXPos = 0;
+                    side = Side.Mid;
+                }
+                continueToMove = false;
+            }
+
+            if(swipeUp)
+            {
+                JumpMouse();
+		        continueToMove = false;
+            }
+        }
+
+
+        // Increase speed
+        if (forwardSpeed < maxSpeed)
+        {
+            forwardSpeed += 0.1f * Time.deltaTime;
+        }
+
+        // Move forward
+        Vector3 moveVector = new Vector3(x - transform.position.x, y * Time.deltaTime, forwardSpeed * Time.deltaTime);
+        x = Mathf.Lerp(x, newXPos, Time.deltaTime * speedDodge);
+        controller.Move(moveVector);
+
+        // Jumping
+        JumpMouse();
+
+        // Sliding
+        if(swipeDown && !slide)
+        {
+            StartCoroutine(SlideMouse());
+        }
+    }
+    
+    private void JumpMouse()
+    {
+        if (controller.isGrounded)
+        {
+            if (swipeUp)
+            {
+                y = jumpPower;
+                jump = true;
+                anim.SetBool("isJump", jump);
+                swipeUp = false;
+            }
+        }
+        else
+        {
+            y -= jumpPower * 2 * Time.deltaTime;
+            jump = false;
+            anim.SetBool("isJump", jump);
+        }
+    }
+
+    private IEnumerator SlideMouse()
+    {
+        slide = true;
+        anim.SetBool("isSlide", slide);
+        controller.center = new Vector3(0, colCenterY / 2f, 0);
+        controller.height = colHeight / 2f;
+
+        yield return new WaitForSeconds(1.3f);
+
+        controller.center = new Vector3(0, colCenterY, 0);
+        controller.height = colHeight;
+        slide = false;
+        anim.SetBool("isSlide", slide);
+    }
+}
