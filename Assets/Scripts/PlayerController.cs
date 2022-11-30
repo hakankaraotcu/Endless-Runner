2619f369b726d676f4c8c06ee33987c6c9e07af2
mbattaloglu <mmbattaloglu@gmail.com>
2022-12-01 00:33:49 +0300

    Contols Resetted

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,16 +10,9 @@ public class PlayerController : MonoBehaviour
     public bool slide = false;
 
     public Side side = Side.Mid;
-    
     private float newXPos = 0f;
-    
     [HideInInspector]
     public bool swipeLeft, swipeRight, swipeUp, swipeDown;
-    [HideInInspector]
-    public bool goLeft, goRight, goUp, goDown;
-    [HideInInspector]
-    public bool continueToMove;
-
     public float xValue;
     public float speedDodge;
     public float jumpPower = 7f;
@@ -30,13 +23,10 @@ public class PlayerController : MonoBehaviour
     private float colHeight;
     private float colCenterY;
 
-    private float x1, x2, y1, y2; 
-
     public Animator anim;
     private CharacterController controller;
 
-    private float leftBorder, rightBorder;
-    
+    // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
@@ -45,72 +35,10 @@ public class PlayerController : MonoBehaviour
         colCenterY = controller.center.y;
         transform.position = Vector3.zero;
     }
-    
+
+    // Update is called once per frame
     void Update()
-    {   
-        if (Input.GetMouseButtonDown(0))
-        {
-            x1 = Input.mousePosition.x;
-            y1 = Input.mousePosition.y;
-            continueToMove = true;
-        }
-
-        if (Input.GetMouseButton(0) && continueToMove)
-        {
-            x2 = Input.mousePosition.x;
-            y2 = Input.mousePosition.y;
-        
-            goRight = x2 - x1 > Screen.width / 5;
-            goLeft = x2 - x1 < 0 && Mathf.Abs(x2 - x1) > Screen.width / 5;
-            goUp = y2 - y1 > Screen.height / 5;
-            goDown = y2 - y1 < 0 && Mathf.Abs(y2 - y1) > Screen.height / 5;
-
-            if (goLeft)
-            {
-                if (side == Side.Mid)
-                {
-                    newXPos = -xValue;
-                    side = Side.Left;
-                    leftBorder = -2;
-                    rightBorder = 0;
-                }
-                else if (side == Side.Right)
-                {
-                    newXPos = 0;
-                    side = Side.Mid;
-                    leftBorder = 0;
-                    rightBorder = -2;
-                }
-                continueToMove = false;
-            }
-
-            else if (goRight)
-            {
-                if (side == Side.Mid)
-                {
-                    newXPos = xValue;
-                    side = Side.Right;
-                    leftBorder = 0;
-                    rightBorder = 2;
-                }
-                else if (side == Side.Left)
-                {
-                    newXPos = 0;
-                    side = Side.Mid;
-                    leftBorder = -2;
-                    rightBorder = 0;
-                }
-                continueToMove = false;
-            }
-
-            if(goUp)
-            {
-                JumpMouse();
-            }
-        }
-
-
-
+    {
         swipeLeft = Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow);
         swipeRight = Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow);
         swipeUp = Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow);
@@ -145,25 +73,23 @@ public class PlayerController : MonoBehaviour
         }
 
         // Increase speed
-        if (forwardSpeed < maxSpeed)
+        if(forwardSpeed < maxSpeed)
         {
             forwardSpeed += 0.1f * Time.deltaTime;
         }
 
         // Move forward
-        x = Mathf.Clamp(Mathf.Lerp(x, newXPos, Time.deltaTime * speedDodge), leftBorder, rightBorder);
         Vector3 moveVector = new Vector3(x - transform.position.x, y * Time.deltaTime, forwardSpeed * Time.deltaTime);
+        x = Mathf.Lerp(x, newXPos, Time.deltaTime * speedDodge);
         controller.Move(moveVector);
 
         // Jumping
-        //Jump();
-        JumpMouse();
+        Jump();
 
         // Sliding
         if(swipeDown && !slide)
         {
-            //StartCoroutine(Slide());
-            StartCoroutine(SlideMouse());
+            StartCoroutine(Slide());
         }
     }
     
@@ -187,39 +113,6 @@ public class PlayerController : MonoBehaviour
     }
 
     private IEnumerator Slide()
-    {
-        slide = true;
-        anim.SetBool("isSlide", slide);
-        controller.center = new Vector3(0, colCenterY / 2f, 0);
-        controller.height = colHeight / 2f;
-
-        yield return new WaitForSeconds(1.3f);
-
-        controller.center = new Vector3(0, colCenterY, 0);
-        controller.height = colHeight;
-        slide = false;
-        anim.SetBool("isSlide", slide);
-    }
-    private void JumpMouse()
-    {
-        if (controller.isGrounded)
-        {
-            if (goUp)
-            {
-                y = jumpPower;
-                jump = true;
-                anim.SetBool("isJump", jump);
-            }
-        }
-        else
-        {
-            y -= jumpPower * 2 * Time.deltaTime;
-            jump = false;
-            anim.SetBool("isJump", jump);
-        }
-    }
-
-    private IEnumerator SlideMouse()
     {
         slide = true;
         anim.SetBool("isSlide", slide);
