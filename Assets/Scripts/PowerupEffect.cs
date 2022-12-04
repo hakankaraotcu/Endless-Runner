dd0fb6f27e0af8034342c378d7c9b28e7f598894
mbattaloglu <mmbattaloglu@gmail.com>
2022-12-04 20:52:47 +0300

    Scriptable Object Fault

--- Assets/Scripts/PowerupEffect.cs
+++ Assets/Scripts/PowerupEffect.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface PowerupEffect : ScriptableObject
+public abstract class PowerupEffect : ScriptableObject
 {
-    void Apply(GameObject target);
+    public abstract void Apply(GameObject target);
 }
